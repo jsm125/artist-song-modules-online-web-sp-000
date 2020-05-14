c4ac92031eaa8cac257f8c09f9f751af83fffc6c
Matt Im <matthew.seiim@gmail.com>
2020-05-14 14:34:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,25 +9,12 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
-
-  # def initialize
-  #   self.class.all << self
-  # end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def self.all
-    @@songs
+    self.class.all
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end

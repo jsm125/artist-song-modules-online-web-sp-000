21ffdc6a41d624aa6d44f4484d5b6e04a2c4984f
Matt Im <matthew.seiim@gmail.com>
2020-05-14 14:29:30 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,14 @@
 module Memorable
   module ClassMethods
     
-  def reset_all
-    self.all.clear
-  end
+    def reset_all
+      self.all.clear
+    end
   
+  end
+
+  module InstanceMethods
+
   def count
     self.all.count
   end
